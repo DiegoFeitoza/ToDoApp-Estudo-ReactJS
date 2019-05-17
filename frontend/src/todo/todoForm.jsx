bd30205a11b70081b6c9205d60596e5cfcdd9f93
Feitoza, Diego <diegofeitoza@yahoo.com.br>
2019-05-17 16:25:07 -0300

    Adicionando REDUX ao projeto

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 
 //Componente GRID monta o grid do bootstrap no padrão do menor para o maior xs sm md lg
 import Grid from '../template/grid'
 import IconButton from '../template/iconButton'
 
-export default props => {
+import { changeDescription } from './todoActions'
+
+const todoForm = props => {
     const keyHandler = (e) => {
         if(e.key === 'Enter'){
             e.shiftKey ? props.handleSearch() : props.handleAdd()
@@ -19,9 +23,9 @@ export default props => {
                 <input id="description"
                         className="form-control"
                         placeholder="Digite a tarefa que deseja adicionar"
-                        onChange={props.handleChange}
+                        onChange={props.changeDescription}
                         onKeyUp={keyHandler}
-                        value={props.value}></input>
+                        value={props.description}></input>
             </Grid>
             <Grid cols="12 3 2">
                 <IconButton style="primary" icon="plus" onClick={props.handleAdd}></IconButton>
@@ -31,3 +35,8 @@ export default props => {
         </div>
     )
 }
+
+const mapStateToProps = state => ({description: state.todo.description})
+const mapDispatchToProps = dispatch => bindActionCreators({ changeDescription }, dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps)(todoForm)
---
+++ frontend/src/todo/todoReducer.js
@@ -0,0 +1,26 @@
+const INITIAL_STATE = {
+    description: 'Teste de input',
+    list: [{
+        _id: 1,
+        description:'Pagar fatura',
+        done: true
+    }, {
+        _id: 2,
+        description: 'Reunião',
+        done: false
+    }, {
+        _id: 3,
+        description: 'Consulta',
+        done: false
+    }]
+}
+
+export default (state= INITIAL_STATE, action) => {
+    switch(action.type){
+        case 'DESCRIPTION_CHANGED': 
+            return {...state, description: action.payload}
+
+        default:
+            return state
+    }
+}
